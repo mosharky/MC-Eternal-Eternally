fd0a06132e6d762444fb2c77d424a31f796ae0df
mo_shark <medoelzarkalt@gmail.com>
2026-06-17 16:26:05 -0400

    yes

---
+++ kubejs/client_scripts/_CUSTOM/quark.js
@@ -0,0 +1,237 @@
+ClientEvents.highPriorityAssets(e => {
+    e.add('quark:attribute_tooltips', {
+        'minecraft:generic.max_health': {
+            display: {
+                mainhand: 'difference',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'difference'
+            },
+            texture: 'quark:attribute/max_health',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.knockback_resistance': {
+            display: {
+                mainhand: 'percentage',
+                offhand: 'percentage',
+                feet: 'percentage',
+                legs: 'percentage',
+                chest: 'percentage',
+                head: 'percentage',
+                potion: 'percentage'
+            },
+            texture: 'quark:attribute/knockback_resistance',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.movement_speed': {
+            display: {
+                mainhand: 'multiplier',
+                offhand: 'multiplier',
+                feet: 'multiplier',
+                legs: 'multiplier',
+                chest: 'multiplier',
+                head: 'multiplier',
+                potion: 'multiplier'
+            },
+            texture: 'quark:attribute/speed',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.attack_damage': {
+            display: {
+                mainhand: 'flat',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'difference'
+            },
+            texture: 'quark:attribute/attack_damage',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.attack_knockback': {
+            display: {
+                mainhand: 'flat',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'difference'
+            },
+            texture: 'quark:attribute/attack_knockback',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.attack_speed': {
+            display: {
+                mainhand: 'flat',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'multiplier'
+            },
+            texture: 'quark:attribute/attack_speed',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.armor': {
+            display: {
+                mainhand: 'flat',
+                offhand: 'flat',
+                feet: 'flat',
+                legs: 'flat',
+                chest: 'flat',
+                head: 'flat',
+                potion: 'flat'
+            },
+            texture: 'quark:attribute/armor',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.armor_toughness': {
+            display: {
+                mainhand: 'flat',
+                offhand: 'flat',
+                feet: 'flat',
+                legs: 'flat',
+                chest: 'flat',
+                head: 'flat',
+                potion: 'flat'
+            },
+            texture: 'quark:attribute/armor_toughness',
+            compare: 'higher_better'
+        },
+        'irons_spellbooks:max_mana': {
+            display: {
+                mainhand: 'difference',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'difference'
+            },
+            texture: 'quark:attribute/max_mana',
+            compare: 'higher_better'
+        },
+        'irons_spellbooks:spell_power': {
+            display: {
+                mainhand: 'multiplier',
+                offhand: 'multiplier',
+                feet: 'multiplier',
+                legs: 'multiplier',
+                chest: 'multiplier',
+                head: 'multiplier',
+                potion: 'multiplier'
+            },
+            texture: 'quark:attribute/spell_power',
+            compare: 'higher_better'
+        },
+        'irons_spellbooks:mana_regen': {
+            display: {
+                mainhand: 'multiplier',
+                offhand: 'multiplier',
+                feet: 'multiplier',
+                legs: 'multiplier',
+                chest: 'multiplier',
+                head: 'multiplier',
+                potion: 'multiplier'
+            },
+            texture: 'quark:attribute/mana_regen',
+            compare: 'higher_better'
+        },
+        'irons_spellbooks:cooldown_reduction': {
+            display: {
+                mainhand: 'multiplier',
+                offhand: 'multiplier',
+                feet: 'multiplier',
+                legs: 'multiplier',
+                chest: 'multiplier',
+                head: 'multiplier',
+                potion: 'multiplier'
+            },
+            texture: 'quark:attribute/cooldown_reduction',
+            compare: 'higher_better'
+        },
+        'minecraft:generic.luck': {
+            display: {
+                mainhand: 'percentage',
+                offhand: 'percentage',
+                feet: 'percentage',
+                legs: 'percentage',
+                chest: 'percentage',
+                head: 'percentage',
+                potion: 'percentage'
+            },
+            texture: 'quark:attribute/luck',
+            compare: 'higher_better'
+        },
+        'forge:swim_speed': {
+            display: {
+                mainhand: 'multiplier',
+                offhand: 'multiplier',
+                feet: 'multiplier',
+                legs: 'multiplier',
+                chest: 'multiplier',
+                head: 'multiplier',
+                potion: 'multiplier'
+            },
+            texture: 'quark:attribute/swim_speed',
+            compare: 'higher_better'
+        },
+        'forge:entity_gravity': {
+            display: {
+                mainhand: 'multiplier',
+                offhand: 'multiplier',
+                feet: 'multiplier',
+                legs: 'multiplier',
+                chest: 'multiplier',
+                head: 'multiplier',
+                potion: 'multiplier'
+            },
+            texture: 'quark:attribute/gravity'
+        },
+        'forge:block_reach': {
+            display: {
+                mainhand: 'difference',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'difference'
+            },
+            texture: 'quark:attribute/reach_distance',
+            compare: 'higher_better'
+        },
+        'forge:entity_reach': {
+            display: {
+                mainhand: 'difference',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'difference'
+            },
+            texture: 'quark:attribute/attack_range',
+            compare: 'higher_better'
+        },
+        'forge:step_height_addition': {
+            display: {
+                mainhand: 'difference',
+                offhand: 'difference',
+                feet: 'difference',
+                legs: 'difference',
+                chest: 'difference',
+                head: 'difference',
+                potion: 'difference'
+            },
+            texture: 'quark:attribute/step_height',
+            compare: 'higher_better'
+        },
+    })
+})
